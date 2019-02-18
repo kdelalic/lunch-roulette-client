87da258541884d1b9a53284df042488325f4a5d6
Karlo Delalic <karlo.delalic1@gmail.com>
2019-02-17 23:48:13 -0500

    Fix test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,66 +1,76 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "../components/App";
-import {mount, shallow, render} from "enzyme";
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import App from '../components/App';
 
-test("renders without crashing", () => {
-    const component = shallow(<App />);
+test('renders without crashing', () => {
+  const component = shallow(<App />);
 
-    expect(component).toMatchSnapshot();
+  expect(component).toMatchSnapshot();
 });
 
-test("should catch if the browser does not support geolocation", () => {
-    global.navigator.geolocation = undefined;
+test('should catch if the browser does not support geolocation', () => {
+  global.navigator.geolocation = undefined;
 
-    const component = shallow(<App />);
+  const component = shallow(<App />);
 
-    const showRestaurantsButton = component.find("#show-restaurants");
-    showRestaurantsButton.simulate("click");
+  const showRestaurantsButton = component.find('#show-restaurants');
+  showRestaurantsButton.simulate('click');
 
-    const message = component
-        .find("h2#message")
-        .text()
-        .trim();
+  const message = component
+    .find('h2#message')
+    .text()
+    .trim();
 
-    expect(message).toEqual("Geolocation is not supported by this browser.");
+  expect(message).toEqual('Geolocation is not supported by this browser.');
 });
 
-describe("fetch restaurant for first time ever", () => {
-    beforeAll(() => {
-        const geolocationMock = {
-            getCurrentPosition: jest.fn(success => {
-                setTimeout(
-                    Promise.resolve(
-                        success({
-                            coords: {
-                                latitude: 43.7888386,
-                                longitude: -79.4375602
-                            }
-                        })
-                    ),
-                    5000
-                );
+describe('fetch restaurant for first time ever', () => {
+  beforeAll(() => {
+    const geolocationMock = {
+      getCurrentPosition: jest.fn(success => {
+        setTimeout(
+          Promise.resolve(
+            success({
+              coords: {
+                latitude: 43.7888386,
+                longitude: -79.4375602
+              }
             })
-        };
-
-        return (global.navigator.geolocation = geolocationMock);
-    });
-
-    it("should display getting geolocation info message", async () => {
-        const component = mount(<App />);
-
-        const showRestaurantsButton = component.find(
-            "WithStyles(Button)#show-restaurants"
+          ),
+          5000
         );
-        showRestaurantsButton.simulate("click");
+      })
+    };
 
-        const message = await component
-            .find("h2#message")
-            .text()
-            .trim();
+    global.navigator.geolocation = geolocationMock;
+  });
 
-        expect(message).toEqual("Getting geolocation info...");
+  it('should display getting geolocation info message', async () => {
+    global.navigator.geolocation.getCurrentPosition = success => {
+      setTimeout(() => {
+        Promise.resolve(
+          success({
+            coords: {
+              latitude: 43.7888386,
+              longitude: -79.4375602
+            }
+          })
+        );
+      }, 2000);
+    };
 
-        component.unmount();
-    });
+    const component = mount(<App />);
+
+    const showRestaurantsButton = component.find('WithStyles(Button)#show-restaurants');
+    showRestaurantsButton.simulate('click');
+
+    const message = await component
+      .find('h2#message')
+      .text()
+      .trim();
+
+    expect(message).toEqual('Getting geolocation info...');
+
+    component.unmount();
+  });
 });
