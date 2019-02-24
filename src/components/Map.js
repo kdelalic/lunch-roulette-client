578ad9ca626051df5bf6ff05d0ed7195406e0506
Karlo Delalic <karlo.delalic@ibm.com>
2019-02-23 22:59:37 -0500

    Small changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import GoogleMapReact from 'google-map-react';
 
 import Marker from './Marker';
 import './Map.css';
 import { DEFAULT_MAP_ZOOM, GOOGLE_MAPS_API_KEY } from '../utils/config';
-import Logger from '../utils/logger';
 
-class Map extends Component {
-  static defaultProps = {
-    restaurantCoords: {},
-    userCoords: {}
-  };
+const Map = props => {
+  const { restaurantCoords, userCoords } = props;
 
-  static propTypes = {
-    restaurantCoords: PropTypes.objectOf(PropTypes.number),
-    userCoords: PropTypes.objectOf(PropTypes.number)
-  };
+  return (
+    <div className="MapComponent">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: GOOGLE_MAPS_API_KEY }}
+        center={[restaurantCoords.latitude, restaurantCoords.longitude]}
+        defaultZoom={DEFAULT_MAP_ZOOM}
+      >
+        <Marker
+          type="restaurant"
+          lat={restaurantCoords.latitude}
+          lng={restaurantCoords.longitude}
+        />
+        <Marker type="user" lat={userCoords.latitude} lng={userCoords.longitude} />
+      </GoogleMapReact>
+    </div>
+  );
+};
 
-  constructor(props) {
-    super(props);
+Map.defaultProps = {
+  restaurantCoords: {},
+  userCoords: {}
+};
 
-    this.logger = new Logger();
-  }
-
-  render() {
-    const { restaurantCoords, userCoords } = this.props;
-
-    return (
-      <div className="MapComponent">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: GOOGLE_MAPS_API_KEY }}
-          center={[restaurantCoords.latitude, restaurantCoords.longitude]}
-          defaultZoom={DEFAULT_MAP_ZOOM}
-        >
-          <Marker restaurant lat={restaurantCoords.latitude} lng={restaurantCoords.longitude} />
-          <Marker user lat={userCoords.latitude} lng={userCoords.longitude} />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-}
+Map.propTypes = {
+  restaurantCoords: PropTypes.objectOf(PropTypes.number),
+  userCoords: PropTypes.objectOf(PropTypes.number)
+};
 
 export default Map;
