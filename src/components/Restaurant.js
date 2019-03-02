2eb88572adc3d4e5e71318ba7b9a33ceea766e57
Karlo Delalic <karlo.delalic@ibm.com>
2019-03-02 01:25:03 -0500

    Add styling, paper

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,16 +1,9 @@
 import React from 'react';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
-import {
-  Card,
-  CardHeader,
-  CardMedia,
-  CardContent,
-  CardActions,
-  IconButton,
-  Chip,
-  Paper
-} from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Chip, Paper } from '@material-ui/core';
+import Money from '@material-ui/icons/AttachMoney';
+import YelpBurst from '../images/yelp_assets/burst/Yelp_burst_positive_RGB.png';
 
 import './Restaurant.css';
 
@@ -27,25 +20,42 @@ const starAssetSrc = rating => {
 };
 
 const Restaurant = props => {
-  const [showMap, setShowMap] = React.useState(false);
-
-  const toggleShowMap = () => {
-    setShowMap(!showMap);
-  };
   const { restaurantInfo } = props;
-  const { name, rating, image_url: imageURL, categories } = restaurantInfo;
+  const { name, rating, image_url: imageURL, categories, url, price } = restaurantInfo;
 
   return (
-    <Paper className="Restaurant">
+    <Paper className="Restaurant" elevation={2}>
       <div className="header">
-        <h2>{name}</h2>
-        <img className="rating" alt={`${rating}/5 Stars`} src={starAssetSrc(rating)} />
+        <h2>
+          {name}
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <img alt="View on Yelp" src={YelpBurst} />
+          </a>
+        </h2>
       </div>
-      <img className="media" src={imageURL} alt={name} />
-      <div className="content">
-        {categories.map(category => {
-          return <Chip label={category.title} className="categoryChip" />;
-        })}
+      <div className="container">
+        <Paper className="media" elevation={1}>
+          <img src={imageURL} alt={name} />
+        </Paper>
+      </div>
+      <div className="container">
+        <Paper className="content" elevation={1}>
+          <img className="rating" alt={`${rating}/5 Stars`} src={starAssetSrc(rating)} />
+          <div className="categories">
+            {categories.map(category => {
+              return <Chip label={category.title} key={category.alias} className="categoryChip" />;
+            })}
+          </div>
+          <div className="price">
+            {price ? (
+              _.times(price.length, i => {
+                return <Money key={i} />;
+              })
+            ) : (
+              <div />
+            )}
+          </div>
+        </Paper>
       </div>
     </Paper>
   );
