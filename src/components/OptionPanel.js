065ab59ef2c889dc8789a7c3a6b82f4edfeb16f9
Karlo Delalic <karlo.delalic1@gmail.com>
2019-03-07 00:40:24 -0500

    Add radius slider

--- src/components/OptionPanel.js
+++ src/components/OptionPanel.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Paper } from '@material-ui/core';
+import Slider from '@material-ui/lab/Slider';
 import PropTypes from 'prop-types';
 
+import { INNER_CONTAINER_ELEVATION, DEFAULT_RADIUS } from '../utils/config';
 import './OptionPanel.css';
 
 const OptionPanel = props => {
   const { getNextRestaurant } = props;
+  const [radius, setRadius] = useState(DEFAULT_RADIUS);
+
+  const handleSlider = (event, value) => {
+    setRadius(value);
+  };
 
   return (
     <Paper className="OptionPanel" elevation={2}>
       <div className="logo">
         <h1>Lunch Roulette</h1>
+      </div>
+      <div className="options">
+        <Paper elevation={INNER_CONTAINER_ELEVATION}>
+          <div className="radiusControl">
+            <span>Radius</span>
+            <Slider
+              className="slider"
+              value={radius}
+              onChange={handleSlider}
+              min={500}
+              max={10000}
+              step={100}
+            />
+          </div>
+        </Paper>
       </div>
       <Button
         className="actionButton shuffleButton"
